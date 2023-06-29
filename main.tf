35bde52ba3091c202719ae04fb068452517f3b79
ganeshs1004 <138108125+ganeshs1004@users.noreply.github.com>
2023-06-29 17:34:45 +0530

    Update main.tf
    added main terraform code in main.tf file

--- main.tf
+++ main.tf
@@ -1,1 +1,96 @@
+# Provider Configuration
+provider "google" {
+  project = "amzu-exercise"
+  region  = "us-central1"
+}
+
+# Resources
+resource "google_compute_network" "network" {
+  name                    = "my-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "my-subnet"
+  region        = "us-central1"
+  network       = google_compute_network.network.name
+  ip_cidr_range = "10.0.0.0/24"
+}
+
+resource "google_compute_instance" "web_application" {
+  name         = "web-application"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.self_link
+    access_config {
+      // Public IP configuration for web-application
+    }
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    # Add user access to VM
+    echo "user1@example.com" >> /opt/users.txt
+    echo "user2@example.com" >> /opt/users.txt
+  EOT
+}
+
+resource "google_compute_instance" "web_service_1" {
+  name         = "web-service-1"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.self_link
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    # Add user access to VM
+    echo "user1@example.com" >> /opt/users.txt
+    echo "user2@example.com" >> /opt/users.txt
+  EOT
+}
+
+resource "google_compute_instance" "web_service_2" {
+  name         = "web-service-2"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.self_link
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    # Add user access to VM
+    echo "user1@example.com" >> /opt/users.txt
+    echo "user2@example.com" >> /opt/users.txt
+  EOT
+}
+
+# Output
+output "public_ip_address" {
+  value = google_compute_instance.web_application.network_interface[0].access_config[0].nat_ip
+}
 
